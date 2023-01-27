02dfc1ef1ef750d6f6f4db5457dc238f8574c835
Ahmad Akel <ahmad.oudai1999@gmail.com>
2023-01-27 16:05:58 +0100

    Second Commit: Create a Color Picker For Drawing And Filling Shapes

--- Maps/src/main/java/com/example/maps/HelloApplication.java
+++ Maps/src/main/java/com/example/maps/HelloApplication.java
@@ -10,10 +10,13 @@ import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage primaryStage)  {
+    public void start(Stage primaryStage) {
         Group root = new Group();
-        Canvas canvas = new Canvas(600, 400);
+        Canvas canvas = new Canvas(1200, 800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setFill(Color.GRAY); // set default fill color to white
+        gc.setLineWidth(2); // set default brush size
 
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
                 event -> gc.beginPath());
@@ -24,10 +27,22 @@ public class HelloApplication extends Application {
         canvas.addEventHandler(MouseEvent.MOUSE_RELEASED,
                 event -> {
                     gc.stroke();
+                    gc.fill(); // fill the shape with the current fill color
                     gc.closePath();
                 });
 
-        root.getChildren().add(canvas);
+        // adding a color picker to change the fill color of the shape
+        javafx.scene.control.ColorPicker fillColorPicker = new javafx.scene.control.ColorPicker();
+        fillColorPicker.setValue(Color.WHITE); // set default fill color
+        fillColorPicker.setOnAction(e -> gc.setFill(fillColorPicker.getValue()));
+        fillColorPicker.setLayoutX(150);
+
+        // adding a color picker to change the stroke color of the shape
+        javafx.scene.control.ColorPicker strokeColorPicker = new javafx.scene.control.ColorPicker();
+        strokeColorPicker.setValue(Color.BLACK); // set default stroke color
+        strokeColorPicker.setOnAction(e -> gc.setStroke(strokeColorPicker.getValue()));
+
+        root.getChildren().addAll(canvas, fillColorPicker, strokeColorPicker);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
